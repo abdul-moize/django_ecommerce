e4e6b4f058ebd6a1ae0df59fd6a94a75480dba18
abdul-moize <abdul.moize@arbisoft.com>
2021-09-21 20:23:21 +0500

    feat: Templates Login & Register

--- users/contants.py
+++ users/contants.py
@@ -14,6 +14,40 @@ ROLES = {
     SALES_MANAGER: "Sales Manager",
     SYSTEM_ADMIN: "System Admin",
 }
+<<<<<<< HEAD
 # urls
 HOME_PAGE_URL = "/admin/"
 AUTH_PAGE_URL = "/"
+=======
+# directory containing css file
+LOGIN_CSS_DIRECTORY = "/login_register/style.css"
+# form actions
+ACTION_LOGIN = "/users/login/"
+ACTION_REGISTER = "/users/"
+# urls
+HOME_PAGE_URL = "/"
+LOGIN_PAGE_URL = "/users/login/"
+REGISTER_PAGE_URL = "/users/login/"
+# Templates
+LOGIN = "login_register/index.html"
+REGISTER = "login_register/index.html"
+LOGIN_SUCCESS = "login_success/index.html"
+LOGIN_FAILURE = "login_failure/index.html"
+REGISTER_SUCCESS = "register_success/index.html"
+REGISTER_FAILURE = "register_failure/index.html"
+# Contexts
+LOGIN_CONTEXT = {
+    "path_css": LOGIN_CSS_DIRECTORY,
+    "action_login": ACTION_LOGIN,
+    "action_register": ACTION_REGISTER,
+}
+REGISTER_CONTEXT = {
+    "path_css": LOGIN_CSS_DIRECTORY,
+    "action_login": ACTION_LOGIN,
+    "action_register": ACTION_REGISTER,
+}
+LOGIN_SUCCESS_CONTEXT = {"home_page": HOME_PAGE_URL}
+LOGIN_FAILURE_CONTEXT = {"login_page": LOGIN_PAGE_URL}
+REGISTER_FAILURE_CONTEXT = {"register_page": REGISTER_PAGE_URL}
+REGISTER_SUCCESS_CONTEXT = {"login_page": LOGIN_PAGE_URL}
+>>>>>>> feat: Templates Login & Register
